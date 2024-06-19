ef4ec361b7327af9c6607ebe5d0391a7d929d96e
James <james.wang@toplogis.com>
2024-06-19 14:35:12 +0800

    update

--- src/components/Onlookers.vue
+++ src/components/Onlookers.vue
@@ -2,14 +2,14 @@
 <template>
   <v-container class="mt-5">
     <v-row justify="center" dense>
-      <v-col class="text-center">
+      <v-col cols="12" class="text-center">
         <h5>選擇您心目中的冠軍隊伍！</h5>
         <h5>吃瓜同時與冠軍一起分瓜～</h5>
         <h5>SLOGAN</h5>
         <h5>備註：如冠軍無人送瓜則瓜瓜直至下期夏至盃！</h5>
         <h5>1瓜 = 50</h5>
       </v-col>
-      <v-col cols="12" class="text-right mb-n4 mt-n4">
+      <v-col cols="12" lg="7" class="text-right mb-n4 mt-n4">
         <v-btn
           variant="text"
           color="green"
