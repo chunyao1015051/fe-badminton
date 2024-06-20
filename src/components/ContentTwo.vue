226c73a289ad987c0a6133a561bb53a1b5798363
AORUS-5-SE\haruh <haruhitokyonsos@gmail.com>
2024-06-20 21:57:53 +0800

    no message

--- src/components/ContentTwo.vue
+++ src/components/ContentTwo.vue
@@ -2,10 +2,7 @@
 <template>
   <v-container class="mt-5">
     <v-row justify="center" dense>
-      <v-col
-        v-if="['James', 'Mega', 'Olivia'].includes(user.name)"
-        class="text-center"
-      >
+      <v-col v-if="['James', 'Mega', 'Olivia'].includes(user.name)" class="text-center">
         <v-btn color="blue" @click="isDialog = true"> 輸入比賽結果 </v-btn>
       </v-col>
       <v-col cols="12">
@@ -23,14 +20,85 @@
         <h3 class="text-center">請選手們移至比賽場地</h3>
         <h3 class="text-center">下一組 C vs D 選手預備</h3>
       </v-col>
-      <v-col cols="12" class="text-center" style="position: relative; top: 96px;margin-top: -90px">
-        <svg width="90" height="90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <v-col
+        cols="12"
+        class="text-center"
+        style="position: relative; top: 96px; margin-top: -90px"
+      >
+        <svg
+          width="100"
+          height="100"
+          style="enable-background: new 0 0 128 128"
+          version="1.1"
+          viewBox="0 0 128 128"
+          xml:space="preserve"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <circle class="st0" cx="64" cy="64" r="60" />
+          <circle class="st1" cx="64" cy="64" r="50" />
+          <path class="st8" d="M84,110H44V92c0-1.1,0.9-2,2-2h36c1.1,0,2,0.9,2,2V110z" />
+          <polygon class="st9" points="87,113 41,113 42,110 86,110 " />
           <path
-            d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5M19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z"
+            class="st23"
+            d="M72,106H56c-2.2,0-4-1.8-4-4v-3c0-2.2,1.8-4,4-4h16c2.2,0,4,1.8,4,4v3C76,104.2,74.2,106,72,106z"
+          />
+          <rect class="st16" height="16.5" width="10.5" x="58.8" y="68.5" />
+          <rect class="st24" height="16.5" width="3" x="66.2" y="68.5" />
+          <path
+            class="st14"
+            d="M73,84H55c-3.3,0-6,2.7-6,6v0h30v0C79,86.7,76.3,84,73,84z"
+          />
+          <path class="st15" d="M73,84h-5c3.3,0,6,2.7,6,6h5C79,86.7,76.3,84,73,84z" />
+          <path
+            class="st14"
+            d="M69.1,77H58.9c-2.7,0-4.9-2.2-4.9-4.9V71h20v1.1C74,74.8,71.8,77,69.1,77z"
+          />
+          <path
+            class="st15"
+            d="M70,71v1.1c0,2.7-1.2,4.9-3.9,4.9h3c2.7,0,4.9-2.2,4.9-4.9V71H70z"
+          />
+          <path
+            class="st16"
+            d="M81,90H47l0,0c0-1.1,0.9-2,2-2h30C80.1,88,81,88.9,81,90L81,90z"
+          />
+          <path class="st24" d="M79,88h-5c1.1,0,2,0.9,2,2h5C81,88.9,80.1,88,79,88z" />
+          <path class="st16" d="M76,69H52v3c0,1.1,0.9,2,2,2h20c1.1,0,2-0.9,2-2V69z" />
+          <path class="st16" d="M76,69H52v3c0,1.1,0.9,2,2,2h20c1.1,0,2-0.9,2-2V69z" />
+          <path class="st24" d="M71,69l-2,3c1,0,1.1,2,0,2h5c1.1,0,2-0.9,2-2v-3H71z" />
+          <path class="st28" d="M78,37h19v1c0,10.5-8.5,19-19,19h0" />
+          <path class="st29" d="M50,37H31v1c0,10.5,8.5,19,19,19h0" />
+          <path
+            class="st14"
+            d="M72.5,72h-17C49.1,72,44,66.9,44,60.5V29h40v31.5C84,66.9,78.9,72,72.5,72z"
+          />
+          <path class="st10" d="M50,32c0,0,0,17,0,20" />
+          <path
+            class="st15"
+            d="M79,29v31.5C79,66.9,73.9,72,67.5,72h5C78.9,72,84,66.9,84,60.5V29H79z"
+          />
+          <polygon class="st16" points="87,29 41,29 44,32 84,32 " />
+          <polygon class="st24" points="81,29 79,32 84,32 87,29 " />
+          <path
+            class="st14"
+            d="M88,25H40c-1.1,0-2,0.9-2,2v0c0,1.1,0.9,2,2,2h48c1.1,0,2-0.9,2-2v0C90,25.9,89.1,25,88,25z"
+          />
+          <path
+            class="st15"
+            d="M88,25h-6c1.1,0,2,0.9,2,2c0,1.1-0.9,2-2,2h6c1.1,0,2-0.9,2-2C90,25.9,89.1,25,88,25z"
+          />
+          <line class="st10" x1="50" x2="50" y1="56.4" y2="58.7" />
+          <path
+            class="st23"
+            d="M48.9,21.4l0.6-1.5c0.2-0.4,0.8-0.4,0.9,0l0.6,1.5c0.5,1.2,1.4,2.1,2.6,2.6l1.5,0.6c0.4,0.2,0.4,0.8,0,0.9  l-1.5,0.6c-1.2,0.5-2.1,1.4-2.6,2.6l-0.6,1.5c-0.2,0.4-0.8,0.4-0.9,0l-0.6-1.5c-0.5-1.2-1.4-2.1-2.6-2.6l-1.5-0.6  c-0.4-0.2-0.4-0.8,0-0.9l1.5-0.6C47.5,23.5,48.5,22.5,48.9,21.4z"
+          />
+          <path
+            class="st15"
+            d="M64,54.5l4.3,2.3c0.6,0.3,1.4-0.2,1.3-0.9L68.7,51l3.5-3.4c0.5-0.5,0.2-1.4-0.5-1.5l-4.8-0.7l-2.2-4.4  c-0.3-0.6-1.2-0.6-1.5,0l-2.2,4.4l-4.8,0.7c-0.7,0.1-1,1-0.5,1.5l3.5,3.4l-0.8,4.8c-0.1,0.7,0.6,1.2,1.3,0.9L64,54.5z"
           />
         </svg>
       </v-col>
-      <v-col cols="12" class="text-center">
+      <v-col cols="12" class="text-center mt-3">
         <svg width="330" height="280" viewBox="50 0 300 300">
           <!-- 上方圓形 -->
 
@@ -164,8 +232,7 @@
                         ['A', 'B', 'C', 'D', 'E', 'F'].filter(
                           (el) =>
                             !group_two ||
-                            (group_two &&
-                              el.charCodeAt(0) < group_two.charCodeAt(0))
+                            (group_two && el.charCodeAt(0) < group_two.charCodeAt(0))
                         )
                       "
                     ></v-select>
@@ -189,8 +256,7 @@
                         ['A', 'B', 'C', 'D', 'E', 'F'].filter(
                           (el) =>
                             !group_one ||
-                            (group_one &&
-                              el.charCodeAt(0) > group_one.charCodeAt(0))
+                            (group_one && el.charCodeAt(0) > group_one.charCodeAt(0))
                         )
                       "
                     ></v-select>
@@ -212,10 +278,7 @@
                       color="green"
                       block
                       :disabled="
-                        !group_one ||
-                        !group_two ||
-                        !group_one_scores ||
-                        !group_two_scores
+                        !group_one || !group_two || !group_one_scores || !group_two_scores
                       "
                       @click="updateScores()"
                     >
@@ -281,15 +344,12 @@ export default {
     },
     async updateScores() {
       try {
-        await axios.post(
-          `http://${process.env.SERVER_HOST}/updateScores/大班`,
-          {
-            group_one: this.group_one,
-            group_two: this.group_two,
-            group_one_scores: this.group_one_scores,
-            group_two_scores: this.group_two_scores,
-          }
-        );
+        await axios.post(`http://${process.env.SERVER_HOST}/updateScores/大班`, {
+          group_one: this.group_one,
+          group_two: this.group_two,
+          group_one_scores: this.group_one_scores,
+          group_two_scores: this.group_two_scores,
+        });
         this.isDialog = false;
         await this.refresh();
       } catch (error) {
